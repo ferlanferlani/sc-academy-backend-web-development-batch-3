8b234bdacb4e7566e57e25ebaa53f9a5f89bf190
ferlanferlani <ferlanferlani.id@gmail.com>
2024-09-14 15:44:22 +0700

    first commit

---
+++ javascript-dasar/script.js
@@ -0,0 +1,45 @@
+// variable
+
+// variable dengan tipe data string
+// const variableSatu = "ini isi variable";
+// console.log(variableSatu);
+
+// variable dengan tipe data integer
+// const varibleDua = 10;
+// console.log(varibleDua);
+
+// variable dengan tipe data desimal
+// const variableTiga = 3.14;
+// console.log(variableTiga);
+
+// variable dengan tipe data boolean
+// const variableEmpat = false;
+// console.log(variableEmpat);
+
+
+
+
+
+
+// function
+let data1 = false;
+let data2 = false;
+
+function coba(data1, data2) {
+    
+    if(data1 === true) {
+        data1 = 'berhasil login!';
+    } else {
+        data1 = 'gagal login!';
+    }
+
+    console.log(data1);
+    
+    
+}
+coba(data1,data2);
+
+
+// perkalian
+console.log(5*5);
+console.log(5+5);
